bc818a1d0a36db4554e4141cf44ecba94f198604
hajzerbela <hajzerbela@gmail.com>
2024-08-18 11:07:33 +0200

    Init repo

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "BelaBlog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "BelaCoding",
+  links: {
+    twitter: "https://twitter.com/jollyshopland",
+    github: "https://github.com/jolbol1",
+    personalSite: "https://jamesshopland.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
